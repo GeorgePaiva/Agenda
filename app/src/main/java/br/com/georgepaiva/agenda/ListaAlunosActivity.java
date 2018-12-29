9f3a6ccf6d4ca8d4bc559c12b62758d4196a7927
George Paiva <george.paiva15@gmail.com>
2018-12-29 18:58:46 -0300

    Criando um botão para inclusão de alunos

--- app/src/main/java/br/com/georgepaiva/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/georgepaiva/agenda/ListaAlunosActivity.java
@@ -12,7 +12,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
 
-        String[] alunos = {"Rafael","Marcelo","Paulo","George"};
+        String[] alunos = {"Rafael","Marcelo","Paulo","George","Paul Allen","Ruan","Roberto","Steve Jobs","Steve Wozniak","Bill Gates","Eistein"};
         ListView listaAlunos = findViewById(R.id.lista_alunos);
         ArrayAdapter<String> adapter =new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
         listaAlunos.setAdapter(adapter);
