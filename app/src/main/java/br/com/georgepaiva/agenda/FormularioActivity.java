376ced0f678fd363629c1f0081d1421f999696fa
George Paiva <george.paiva15@gmail.com>
2018-12-29 17:16:37 -0300

    Tratando o clique no botão

--- app/src/main/java/br/com/georgepaiva/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/georgepaiva/agenda/FormularioActivity.java
@@ -2,6 +2,9 @@ package br.com.georgepaiva.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+        Button botaoSalvar = findViewById(R.id.formulario_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this, "Botão clicado!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
