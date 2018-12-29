802a72371fb8fdb23686310a5ad26a80aad99e02
George Paiva <george.paiva15@gmail.com>
2018-12-29 16:10:11 -0300

    Exibindo a lista de alunos

--- app/src/main/java/br/com/georgepaiva/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/georgepaiva/agenda/ListaAlunosActivity.java
@@ -1,7 +1,9 @@
 package br.com.georgepaiva.agenda;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ListaAlunosActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Rafael","Marcelo","Paulo","George"};
+        ListView listaAlunos = findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter =new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
     }
 }
