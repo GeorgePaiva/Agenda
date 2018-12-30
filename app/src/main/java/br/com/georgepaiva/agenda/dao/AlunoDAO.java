3ab32d0a1146e467b935a77fb708ef9419510ec9
George Paiva <george.paiva15@gmail.com>
2018-12-30 14:43:35 -0300

    Implementando metodos onCreate e onUpgrade

---
+++ app/src/main/java/br/com/georgepaiva/agenda/dao/AlunoDAO.java
@@ -0,0 +1,25 @@
+package br.com.georgepaiva.agenda.dao;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AlunoDAO extends SQLiteOpenHelper {
+
+    public AlunoDAO(Context context) {
+        super(context, "Agenda", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String sql = "CREATE TABLE Alunos (id INTEGER PRIMARY KEY, nome TEXT NOT NULL, endereco TEXT, telefone TEXT, email TEXT, nota REAL);";
+        db.execSQL(sql);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String sql = "DROP TABLE IF EXISTS Alunos";
+        db.execSQL(sql);
+        onCreate(db);
+    }
+}
