8d36ce63de75f14b60d40c8d8c6956a0117bcd8b
George Paiva <george.paiva15@gmail.com>
2019-01-12 15:29:33 -0300

    Invocando a câmera

--- app/src/main/java/br/com/georgepaiva/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/georgepaiva/agenda/FormularioActivity.java
@@ -1,12 +1,17 @@
 package br.com.georgepaiva.agenda;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+
+import java.io.File;
 
 import br.com.georgepaiva.agenda.dao.AlunoDAO;
 import br.com.georgepaiva.agenda.modelo.Aluno;
@@ -19,18 +24,25 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
-
-
         helper = new FormularioHelper(this);
-
         Intent intent = getIntent ();
         Aluno aluno = (Aluno) intent.getSerializableExtra ( "aluno");
         if (aluno!= null) {
             helper.preencheFormulario(aluno);
         }
 
+        final View botaoFoto = findViewById ( R.id.formulario_botao_foto );
+        botaoFoto.setOnClickListener ( new View.OnClickListener () {
+            @Override
+            public void onClick(View v) {
+                Intent intentCamera = new Intent ( MediaStore.ACTION_IMAGE_CAPTURE );
+                String caminhoFoto = getExternalFilesDir ( null ) + "/" + System.currentTimeMillis () + ".jpg";
+                File arquivoFoto = new File ( caminhoFoto );
+                intentCamera.putExtra ( MediaStore.EXTRA_OUTPUT, Uri.fromFile ( arquivoFoto ) );
+                startActivity ( intentCamera );
+            }
+        } );
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater= getMenuInflater();
@@ -43,7 +55,6 @@ public class FormularioActivity extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.menu_formulario_ok:
                 Aluno aluno = helper.pegarAluno();
-
                 AlunoDAO dao = new AlunoDAO(this);
                 if(aluno.getId ()!=null){
                     dao.altera(aluno);
@@ -55,7 +66,6 @@ public class FormularioActivity extends AppCompatActivity {
                 finish();
                 break;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
