774eaf9d7fbe29533503a1b826bd21528fa0be1b
George Paiva <george.paiva15@gmail.com>
2019-01-12 11:39:08 -0300

    Implementando permissoes

--- app/src/main/java/br/com/georgepaiva/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/georgepaiva/agenda/ListaAlunosActivity.java
@@ -1,9 +1,12 @@
 package br.com.georgepaiva.agenda;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.Browser;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.ContextMenu;
 import android.view.MenuItem;
@@ -16,6 +19,7 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.List;
+
 import br.com.georgepaiva.agenda.dao.AlunoDAO;
 import br.com.georgepaiva.agenda.modelo.Aluno;
 
@@ -25,79 +29,107 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_lista_alunos);
+        super.onCreate ( savedInstanceState );
+        setContentView ( R.layout.activity_lista_alunos );
 
-        listaAlunos = findViewById(R.id.lista_alunos);
+        listaAlunos = findViewById ( R.id.lista_alunos );
 
         listaAlunos.setOnItemClickListener ( new AdapterView.OnItemClickListener () {
             @Override
             public void onItemClick(AdapterView<?> lista, View item, int position, long id) {
                 Aluno aluno = (Aluno) listaAlunos.getItemAtPosition ( position );
                 Intent intentVaiProFormulario = new Intent ( ListaAlunosActivity.this, FormularioActivity.class );
-                intentVaiProFormulario.putExtra ("aluno", aluno );
-                startActivity (intentVaiProFormulario);
+                intentVaiProFormulario.putExtra ( "aluno", aluno );
+                startActivity ( intentVaiProFormulario );
             }
         } );
 
-        Button novoAluno = findViewById(R.id.novo_aluno);
-        novoAluno.setOnClickListener(new View.OnClickListener() {
+        Button novoAluno = findViewById ( R.id.novo_aluno );
+        novoAluno.setOnClickListener ( new View.OnClickListener () {
             @Override
             public void onClick(View v) {
-                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
-                startActivity(intentVaiProFormulario);
+                Intent intentVaiProFormulario = new Intent ( ListaAlunosActivity.this, FormularioActivity.class );
+                startActivity ( intentVaiProFormulario );
 
             }
-        });
-        registerForContextMenu(listaAlunos);
+        } );
+        registerForContextMenu ( listaAlunos );
 
     }
 
     private void carregaLista() {
-        AlunoDAO dao = new AlunoDAO(this);
-        List<Aluno> alunos = dao.buscaAlunos();
-        dao.close();
+        AlunoDAO dao = new AlunoDAO ( this );
+        List<Aluno> alunos = dao.buscaAlunos ();
+        dao.close ();
 
 
-
-        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno>(this, android.R.layout.simple_list_item_1, alunos);
-        listaAlunos.setAdapter(adapter);
+        ArrayAdapter<Aluno> adapter = new ArrayAdapter<Aluno> ( this, android.R.layout.simple_list_item_1, alunos );
+        listaAlunos.setAdapter ( adapter );
     }
 
     @Override
     protected void onResume() {
-        super.onResume();
-        carregaLista();
+        super.onResume ();
+        carregaLista ();
 
     }
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
         AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition (info.position);
+        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition ( info.position );
+
+        MenuItem itemLigar = menu.add ( "Ligar" );
+        itemLigar.setOnMenuItemClickListener ( new MenuItem.OnMenuItemClickListener () {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                if (ActivityCompat.checkSelfPermission ( ListaAlunosActivity.this, Manifest.permission.CALL_PHONE )
+                        != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions ( ListaAlunosActivity.this,
+                            new String[]{Manifest.permission.CALL_PHONE}, 123 );
+                } else {
+                    Intent intentLigar = new Intent ( Intent.ACTION_CALL );
+                    intentLigar.setData ( Uri.parse ( "tel:" + aluno.getTelefone () ) );
+                    startActivity ( intentLigar );
+                }
+                return false;
+            }
+        } );
+
+        MenuItem itemSMS = menu.add ( "Enviar SMS" );
+        Intent intentSMS = new Intent ( Intent.ACTION_VIEW );
+        intentSMS.setData ( Uri.parse ( "sms:" + aluno.getTelefone () ) );
+        itemSMS.setIntent ( intentSMS );
+
+
+        MenuItem itemMapa = menu.add ( "Visualizar no mapa" );
+        Intent intentMapa = new Intent ( Intent.ACTION_VIEW );
+        intentMapa.setData ( Uri.parse ( "geo:0,0?q=" + aluno.getEndereco () ) );
+        itemMapa.setIntent ( intentMapa );
+
 
         String site = aluno.getEmail ();
-        if (!site.startsWith("http://")){
+        if (!site.startsWith ( "http://" )) {
             site = "http://" + site;
         }
 
         MenuItem itemSite = menu.add ( "Visitar site" );
-        Intent intentSite = new Intent ( Intent.ACTION_VIEW);
-        intentSite.setData ( Uri.parse (site));
+        Intent intentSite = new Intent ( Intent.ACTION_VIEW );
+        intentSite.setData ( Uri.parse ( site ) );
         itemSite.setIntent ( intentSite );
 
-        MenuItem deletar = menu.add("Deletar");
+        MenuItem deletar = menu.add ( "Deletar" );
         deletar.setOnMenuItemClickListener ( new MenuItem.OnMenuItemClickListener () {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
                 AlunoDAO dao = new AlunoDAO ( ListaAlunosActivity.this );
-                dao.deleta(aluno);
+                dao.deleta ( aluno );
                 dao.close ();
 
                 carregaLista ();
                 return false;
             }
-        });
+        } );
     }
 
 }
