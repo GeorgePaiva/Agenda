99d14c57f6e5a873b42cfacf3d3d6a88327b8c77
George Paiva <george.paiva15@gmail.com>
2019-01-13 10:37:55 -0300

    Reaproveitando as views no adapter

--- app/src/main/java/br/com/georgepaiva/agenda/adapter/AlunosAdapter.java
+++ app/src/main/java/br/com/georgepaiva/agenda/adapter/AlunosAdapter.java
@@ -1,14 +1,19 @@
 package br.com.georgepaiva.agenda.adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
 
 import br.com.georgepaiva.agenda.ListaAlunosActivity;
+import br.com.georgepaiva.agenda.R;
 import br.com.georgepaiva.agenda.modelo.Aluno;
 
 public class AlunosAdapter extends BaseAdapter {
@@ -37,9 +42,31 @@ public class AlunosAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        TextView view = new TextView(context);
         Aluno aluno = alunos.get(position);
-        view.setText(aluno.toString());
+
+        LayoutInflater  inflater = LayoutInflater.from (context);
+        
+        View view = convertView;
+        if (view == null){
+            view = inflater.inflate (R.layout.list_item, parent, false);
+        }
+
+
+        TextView campoNome = view.findViewById ( R.id.item_nome );
+        campoNome.setText(aluno.getNome ());
+
+        TextView campoTelefone = view.findViewById ( R.id.item_telefone );
+        campoTelefone.setText (aluno.getTelefone());
+
+        ImageView campoFoto = view.findViewById ( R.id.item_foto );
+        String caminhoFoto = aluno.getCaminhoFoto ();
+        if (caminhoFoto!= null) {
+            Bitmap bitmap = BitmapFactory.decodeFile ( caminhoFoto );
+            Bitmap bitmapReduzido = Bitmap.createScaledBitmap ( bitmap, 100, 100, true );
+            campoFoto.setImageBitmap ( bitmapReduzido );
+            campoFoto.setScaleType ( ImageView.ScaleType.FIT_XY );
+        }
+
         return view;
     }
 }
